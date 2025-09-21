a2d13aeed8e4ee8089f8021c6769039e4d52a0d2
DTSmth <ducktapedave85@gmail.com>
2025-09-21 14:37:29 -0400

    edit sprite shape spline. add boost ability to player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,29 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float torqueAmount = 8f;
+    [SerializeField] float baseSpeed = 22f;
+    [SerializeField] float boostSpeed = 29;
     InputAction moveAction;
     Rigidbody2D myRigidBody2D;
+    SurfaceEffector2D surfaceEffector2D;
+    Vector2 moveVector;
+
     void Start()
     {
         moveAction = InputSystem.actions.FindAction("Move");
         myRigidBody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     void Update()
     {
-        Vector2 moveVector;
+
+        RotatePlayer();
+        BoostPlayer();
+    }
+
+    void RotatePlayer()
+    {
         moveVector = moveAction.ReadValue<Vector2>();
         if (moveVector.x < 0)
         {
@@ -24,6 +36,17 @@ public class PlayerController : MonoBehaviour
         {
             myRigidBody2D.AddTorque(-torqueAmount);
         }
-        
+    }
+
+    void BoostPlayer()
+    {
+        if (moveVector.y > 0)
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
     }
 }
